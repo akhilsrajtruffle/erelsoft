13e2978782a7b5d730fc7672e4b456e618931c80
Ozgen <ozgn45@gmail.com>
2019-03-28 00:09:00 +0300

    The first commit

--- erelsoft/ViewController.swift
+++ erelsoft/ViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, WKNavigationDelegate  {
+    
+var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // 1
+        let url = URL(string: "https://erelsoft.com")!
+        webView.load(URLRequest(url: url))
+        
+        // 2
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        toolbarItems = [refresh]
+        navigationController?.isToolbarHidden = false
+    }
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
     }
 
 
