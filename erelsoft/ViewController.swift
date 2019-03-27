036ed0af81e1d9a2799d51021a9b3911ec52ed3b
Ozgen <ozgn45@gmail.com>
2019-03-27 23:29:49 +0300

    Initial Commit

---
+++ erelsoft/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  erelsoft
+//
+//  Created by Mehmet Ozgen on 3/27/19.
+//  Copyright © 2019 Mehmet Ozgen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
